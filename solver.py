972ce9e930704fc33610d85b631adad2c03fe33d
Colman Koivisto <koivisto@cs.ualberta.ca>
2020-10-19 19:31:50 -0600

    second

--- solver.py
+++ solver.py
@@ -35,6 +35,70 @@ class Solver:
         return code
 
 
+    def win_in_two(self, state, color):
+        """
+        Returns BLACK or WHITE if any five in a row is detected for the color
+        EMPTY otherwise.
+        """
+        for r in self.board.rows:
+            result = self.board.has_five_in_list(r)
+            if result != EMPTY:
+                return result
+        for c in self.board.cols:
+            result = self.board.has_five_in_list(c)
+            if result != EMPTY:
+                return result
+        for d in self.board.diags:
+            result = self.board.has_five_in_list(d)
+            if result != EMPTY:
+                return result
+        return EMPTY
+
+
+
+    def win_in_two_in_list(self, list):
+        """
+        1 1 0 1 1
+        Returns BLACK or WHITE if any five in a rows exist in the list.
+        EMPTY otherwise.
+        """
+
+        prev = BORDER
+        prev2 = BORDER
+        counter = 1
+        for stone in list:
+            if self.get_color(stone) == prev:
+                counter += 1
+            else:
+                counter = 1
+                prev = self.get_color(stone)
+            if counter == 5 and prev != EMPTY:
+                return prev
+        return EMPTY
+
+        '''
+        prev = BORDER
+        counter = 1
+        color = EMPTY
+        emptyCounter = 0
+        for stone in list:
+            thisColor = self.get_color(stone)
+            if thisColor != EMPTY:
+                color = thisColor
+            else:
+                emptyCounter += 1
+            if thisColor == prev or thisColor == EMPTY:
+                counter += 1
+            else:
+                counter = 1
+                prev = thisColor
+            if counter == 5 and prev != EMPTY and emptyCounter < 3:
+                return prev
+
+        return EMPTY
+        '''
+
+
 
     def sortedBest(self, state, color):
         opponent = GoBoardUtil.opponent(color)
@@ -61,7 +125,6 @@ class Solver:
                 best.insert(0, m)
                 continue
 
-            '''
             #Check win in two moves
             newState = state.copy()
             newState.play_move(m, color)
@@ -72,11 +135,12 @@ class Solver:
                 result = newState2.detect_five_in_a_row()
                 if result == color:
                     best.insert(0, m)
+                    best.insert(0, m2)
                     found = True
                     break
             if found:
                 continue
-            '''
+
 
             best.append(m)
 
